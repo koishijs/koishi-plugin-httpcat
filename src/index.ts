9a10c92e68254c12d6a138c8cdd3457710e11d39
Seidko <64234553+Seidko@users.noreply.github.com>
2023-05-03 19:29:32 +0800

    fix: code exist but send invalid

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,4 @@
-import { Context, Schema, segment } from 'koishi'
-
-const STATUS_CODES = [
-  100, 101,
-  200, 201, 202, 204, 206, 207,
-  300, 301, 302, 303, 304, 305, 307,
-  400, 401, 402, 403, 404, 405, 406, 408,
-  409, 410, 411, 412, 413, 414, 415, 416,
-  417, 418, 420, 421, 422, 423, 424, 425,
-  426, 429, 431, 444, 450, 451, 499,
-  500, 501, 502, 503, 504, 505, 506, 507,
-  508, 509, 510, 511, 599,
-]
+import { Context, Schema, segment, Quester } from 'koishi'
 
 export const name = 'httpcat'
 
@@ -22,8 +10,15 @@ export function apply(ctx: Context) {
   ctx.i18n.define('zh', require('./locales/zh'))
 
   ctx.command('httpcat <code:number>', { checkArgCount: true })
-    .action(async ({ session }, code) => {
-      if (!STATUS_CODES.includes(code)) return session.text('.invalid', [code])
-      return segment.image(`https://http.cat/${code}`)
+    .action(async ({}, code) => {
+      try {
+        const buffer = await ctx.http.get<ArrayBuffer>(`https://http.cat/${code}`, {
+          responseType: 'arraybuffer',
+        })
+        return segment.image(buffer, 'image/jpeg')
+      } catch (e) {
+        if (!Quester.isAxiosError(e) || e.code !== '404') throw e
+        return segment.i18n('.invalid', [code])
+      }
     })
 }
