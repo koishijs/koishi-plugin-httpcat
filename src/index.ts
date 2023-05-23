211c2f56124643979ff17c076808169d1972894f
Shigma <1700011071@pku.edu.cn>
2023-05-23 17:06:45 +0800

    Update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ export function apply(ctx: Context) {
   ctx.i18n.define('zh', require('./locales/zh'))
 
   ctx.command('httpcat <code:number>', { checkArgCount: true })
-    .action(async ({ session}, code) => {
+    .action(async ({ session }, code) => {
       const url = `https://http.cat/${code}`
       try {
         await ctx.http.head(url)
