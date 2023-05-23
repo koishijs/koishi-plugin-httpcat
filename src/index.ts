f3b54f166f269ac4f4b5df2d50b20d9418a6a51c
Seidko <seidko.coding@gmail.com>
2023-05-23 17:07:14 +0800

    fix: code exist but send invalid (#1)
    Co-authored-by: Shigma <1700011071@pku.edu.cn>

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,4 @@
-import { Context, Schema, segment } from 'koishi'
-
-const STATUS_CODES = [
-  100, 101,
-  200, 201, 202, 204, 206, 207,
-  300, 301, 302, 303, 304, 305, 307,
-  400, 401, 402, 403, 404, 405, 406, 408,
-  409, 410, 411, 412, 413, 414, 415, 416,
-  417, 418, 420, 421, 422, 423, 424, 425,
-  426, 429, 431, 444, 450, 451, 499,
-  500, 501, 502, 503, 504, 505, 506, 507,
-  508, 509, 510, 511, 599,
-]
+import { Context, Schema, segment, Quester } from 'koishi'
 
 export const name = 'httpcat'
 
@@ -23,7 +11,13 @@ export function apply(ctx: Context) {
 
   ctx.command('httpcat <code:number>', { checkArgCount: true })
     .action(async ({ session }, code) => {
-      if (!STATUS_CODES.includes(code)) return session.text('.invalid', [code])
-      return segment.image(`https://http.cat/${code}`)
+      const url = `https://http.cat/${code}`
+      try {
+        await ctx.http.head(url)
+      } catch (e) {
+        if (Quester.isAxiosError(e) && e?.response?.status === 404) return session.text('.invalid', [code])
+        throw e
+      }
+      return segment.image(url)
     })
 }
